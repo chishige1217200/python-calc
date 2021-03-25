a31c36aae159d3bc70550b9ea89dfaf65cb5decf
chishige1217200 <daichicchi01@gmail.com>
2021-03-25 15:31:10 +0900

    [update] 機能縮小・コメント追加

--- calc.py
+++ calc.py
@@ -1,71 +1,30 @@
 out = 0  # 計算結果
 typ = 0  # 計算用の入力数値
 mode = 0  # mode選択用
-flag = 0  # y or n フラグ用
-checkflag = -1  # typの入力の是非(-1: 初回時のみ)
-his_operand = 0  # 履歴を1回分保存
-his_num = 0  # 履歴を1回分保存
 
 print("Starting calculator...\n", end='')
 while True:
-    print("Please select the calc mode. (\"+\" or \"-\" or \"*\" or \"/\" or \"0\" or \"c\" or \"h\" or \"q\")\nMode? : ", end='')
-    mode = input()
-    if mode == "q":
+    print("Please select the calc mode. (\"+\" or \"-\" or \"*\" or \"/\" or \"q\")\nMode? : ", end='')
+    mode = input()              # モード選択
+    if mode == "q":             # 'q'を入力したら終了．30行目にジャンプ
         break
-    if mode == "0":
-        print("Do you want to reset calculation result? (y or N)\n", end='')
-        flag = input()
-        if flag == "y":
-            print("Reset calculation result.\n\n", end='')
-            out = 0
-        else:
-            print("Operation Cancelled.\n\n", end='')
 
-        continue
-
-    if mode == "c":
-        print("Result : " + str(out) + "\n\n", end='')
-        continue
-
-    if mode == "h":
-        if checkflag == -1:
-            print("Cannot use history func before calculating once.\n\n", end='')
-            continue
-
-        print("Do you want to calc " + his_operand +
-              str(his_num) + " again? (y or N)\n", end='')
-        flag = input()
-        if flag == "y":
-            print("Calculated " + his_operand +
-                  str(his_num) + " again.\n", end='')
-            mode = his_operand
-            typ = his_num
-            checkflag = 1
-        else:
-            print("Operation cancelled.\n", end='')
-            continue
-
-    if mode != "+" and mode != "-" and mode != "*" and mode != "/":
+    if mode != "+" and mode != "-" and mode != "*" and mode != "/":  # 存在しない演算モードの場合
         print("Please select the correct mode.\n\n", end='')
-        continue
+        continue                # これより下の処理を無視して，ループ先頭に戻る
 
-    if checkflag <= 0:
-        print("Please input the number.(int type ONLY)\nNumber? : ", end='')
-        typ = input()
+    print("Please input the number.(int type ONLY)\nNumber? : ", end='')
+    typ = input()               # 数値入力
 
-    his_operand = mode
-    his_num = typ
-
-    if mode == "+":
+    if mode == "+":             # 足し算
         out = out + int(typ)
-    if mode == "-":
+    if mode == "-":             # 引き算
         out = out - int(typ)
-    if mode == "*":
+    if mode == "*":             # 掛け算
         out = out * int(typ)
-    if mode == "/":
-        out = out / int(typ)  # 0除算の禁止機能は未実装
+    if mode == "/":             # 割り算
+        out = out / int(typ)
 
     print("Result : " + str(out) + "\n\n", end='')
-    checkflag = 0
-
+# end while
 print("FINAL RESULT : " + str(out) + "\nQuit\n", end='')
